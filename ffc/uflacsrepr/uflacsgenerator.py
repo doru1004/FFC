3b7262415ae5a8e19db5672b9e7bc293226a5d77
Fabio Luporini <f.luporini12@imperial.ac.uk>
2014-11-05 18:58:26 +0000

    Wrap the body returned by ufl

--- ffc/uflacsrepr/uflacsgenerator.py
+++ ffc/uflacsrepr/uflacsgenerator.py
@@ -29,9 +29,16 @@ def generate_integral_code(ir, prefix, parameters):
     code = initialize_integral_code(ir, prefix, parameters)
 
     # Generate tabulate_tensor body using uflacs algorithms
-    uflacs_code = generate_tabulate_tensor_code(ir, prefix, parameters)
+    uflacs_code = generate_tabulate_tensor_code(ir, parameters)
+    tabulate_tensor_code = \
+"""
+void %s(%s)
+{
+%s
+}
+""" % (code['classname'], uflacs_code["tabulate_tensor_arglist"], uflacs_code["tabulate_tensor"])
 
-    code["tabulate_tensor"] = uflacs_code["tabulate_tensor"]
+    code["tabulate_tensor"] = tabulate_tensor_code
 
     # TODO: Use code generation utils here for consistency
     if ir.get("num_cells") is not None:
