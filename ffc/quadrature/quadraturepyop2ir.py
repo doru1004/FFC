0db4be85c5ba49e8a2b18900f05062e0d30823bc
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-05-21 16:01:39 +0100

    Match new coffee interface for array initializers

--- ffc/quadrature/quadraturepyop2ir.py
+++ ffc/quadrature/quadraturepyop2ir.py
@@ -349,15 +349,11 @@ def _tabulate_tensor(ir, parameters):
         zeroflags = values.get_zeros()
         feo_sym = pyop2.Symbol(name, rank)
         init = pyop2.ArrayInit(values, precision)
-        pragma = ""
         if zeroflags is not None and not zeroflags.all():
             nz_indices = [i for i, j in enumerate(zeroflags.tolist()) if not j] or [-1]
-            nz_bounds = (nz_indices[0], nz_indices[-1])
-            pragma = "#pragma coffee nonzerocolumns(%d, %d)" % nz_bounds
-            init = pyop2.ColSparseArrayInit(values, precision, nz_bounds)
-        pyop2_basis.append(pyop2.Decl("double", feo_sym, init,
-                                      qualifiers=["static", "const"],
-                                      pragma=pragma))
+            nz_bounds = tuple((0, i-1) for i in rank[:-1]) + ((nz_indices[0], nz_indices[-1]),)
+            init = pyop2.SparseArrayInit(values, precision, nz_bounds)
+        pyop2_basis.append(pyop2.Decl("double", feo_sym, init, ["static", "const"]))
 
     # Build the root of the PyOP2' ast
     pyop2_tables = pyop2_weights + [tab for tab in pyop2_basis]
