d1714425428cd2a55f94d5e7fe9950486ff8c860
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2015-11-16 11:00:10 +0000

    Unfix bork

--- ffc/quadrature_schemes.py
+++ ffc/quadrature_schemes.py
@@ -60,7 +60,7 @@ def create_quadrature(cell, degree, scheme="default"):
 
     # FIXME: KBO: Can this be handled more elegantly?
     # Handle point case
-    if isinstance(cell, int) and cell == 0 or cellname2dim(cell.cellname()) == 0:
+    if isinstance(cell, int) and cell == 0 or cellname2dim(cell) == 0:
         return ([()], array([1.0,]))
         
     if isinstance(cell, str):
